ba466ede2f5d447ea39593994e7f5a9f11ed7347
yhu04 <constance.woo58@gmail.com>
2016-07-11 14:40:34 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
+chmod u+x run.sh
 python ./src/median_degree.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
 
